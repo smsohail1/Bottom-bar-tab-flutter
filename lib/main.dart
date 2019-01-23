a9f8c13c2afe8b2fc73519bad62e20e4e1324814
Muhammad_sohail <syedmuhammadsohail@yahoo.com>
2019-01-23 16:50:02 +0500

    Bottom navigation tabbar

---
+++ lib/main.dart
@@ -0,0 +1,299 @@
+import 'package:beautiful_list/model/lesson.dart';
+import 'package:flutter/material.dart';
+import 'package:beautiful_list/detail_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+          primaryColor: Color.fromRGBO(58, 66, 86, 1.0), fontFamily: 'Raleway'),
+      //home: new ListPage(title: 'Lessons'),
+      home: TabLayoutDemo(),
+      // home: DetailPage(),
+    );
+  }
+}
+
+class TabLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      color: Colors.yellow,
+      home: DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          backgroundColor: Color.fromRGBO(198, 29, 29, 1.0),
+          body: TabBarView(
+            children: [
+              new Container(
+                color: Colors.yellow,
+              ),
+              new Container(
+                color: Colors.orange,
+              ),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+
+            indicator: UnderlineTabIndicator(borderSide: const BorderSide(width: 2.0, color: Colors.white)),
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home, color: Colors.white),
+              ),
+              Tab(
+                icon: new Icon(Icons.rss_feed, color: Colors.white),
+              ),
+              Tab(
+                icon: new Icon(Icons.perm_identity, color: Colors.white),
+              ),
+              Tab(
+                icon: new Icon(Icons.settings, color: Colors.white),
+              )
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ListPage extends StatefulWidget {
+  ListPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  List lessons;
+
+  @override
+  void initState() {
+    lessons = getLessons();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ListTile makeListTile(Lesson lesson) => ListTile(
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: new BoxDecoration(
+                border: new Border(
+                    right: new BorderSide(width: 1.0, color: Colors.white24))),
+            child: Icon(Icons.autorenew, color: Colors.white),
+          ),
+          title: Text(
+            lesson.title,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+          subtitle: Row(
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    // tag: 'hero',
+                    child: LinearProgressIndicator(
+                        backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                        value: lesson.indicatorValue,
+                        valueColor: AlwaysStoppedAnimation(Colors.green)),
+                  )),
+              Expanded(
+                flex: 4,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(lesson.level,
+                        style: TextStyle(color: Colors.white))),
+              )
+            ],
+          ),
+          trailing:
+              Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailPage(lesson: lesson)));
+          },
+        );
+
+    Card makeCard(Lesson lesson) => Card(
+          elevation: 8.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+            child: makeListTile(lesson),
+          ),
+        );
+
+    final makeBody = Container(
+      // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: lessons.length,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(lessons[index]);
+        },
+      ),
+    );
+
+    final bottomTabBar = BottomNavigationBar(
+      currentIndex: 0, // this will be set when a new tab is tapped
+      items: [
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.home),
+          title: new Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.mail),
+          title: new Text('Messages'),
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person), title: Text('Profile'))
+      ],
+    );
+
+    final makeBottom = Container(
+      height: 55.0,
+      child: BottomAppBar(
+        color: Color.fromRGBO(58, 66, 86, 1.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.white),
+              splashColor: Colors.green,
+              disabledColor: Colors.red,
+              color: Colors.amber,
+              highlightColor: Colors.blue,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.blur_on, color: Colors.white),
+              splashColor: Colors.green,
+              disabledColor: Colors.red,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.hotel, color: Colors.white),
+              splashColor: Colors.green,
+              disabledColor: Colors.red,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.account_box, color: Colors.white),
+              splashColor: Colors.green,
+              disabledColor: Colors.red,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+    final topAppBar = AppBar(
+      elevation: 0.1,
+      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      title: Text(widget.title),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.list),
+          onPressed: () {},
+        )
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(200, 200, 200, 1.0),
+      appBar: topAppBar,
+      body: makeBody,
+      // bottomNavigationBar: makeBottom,
+      bottomNavigationBar: bottomTabBar,
+    );
+  }
+}
+
+List getLessons() {
+  return [
+    Lesson(
+        title: "Introduction to Driving",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 20,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Observation at Junctions",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Reverse parallel Parking",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Reversing around the corner",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Incorrect Use of Signal",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Engine Challenges",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        title: "Self Driving Car",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed.  ")
+  ];
+}
